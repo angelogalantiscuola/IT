0c0a5fa0aaad5540b26934bb383087b51d22c323
angelogalantiscuola <91282441+angelogalantiscuola@users.noreply.github.com>
2022-12-22 12:31:19 +0100

    read_file and path

---
+++ path.py
@@ -0,0 +1,15 @@
+# PERCORSO ASSOLUTO E PERCORSO RELATIVO
+
+# # cartella in cui mi trovo
+# 'D:\Informatica\roberto'
+# # percorso assoluto
+# 'D:\Informatica\roberto\pluto\pippo.txt'
+# # percorso relativo (alla cartella in cui mi trovo)
+# '.\pluto\pippo.txt'
+# '\n \t \r' # backslash
+# '/' # slash
+# per i path si usa lo slash
+# windows notation
+# file_path = 'D:\Informatica\roberto\pluto\pippo.txt'
+# linux notation
+# file_path = 'D:/Informatica/roberto/pluto/pippo.txt'
