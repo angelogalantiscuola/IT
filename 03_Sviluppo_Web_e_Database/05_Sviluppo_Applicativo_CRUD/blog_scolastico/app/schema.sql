cbc9086702fc9b6bad606ac395662b54e497e7b2
Angelo Galanti <angelo.galanti@isissgobetti.it>
2025-12-16 10:20:43 +0100

    Add initial data entries for users and posts in schema.sql

--- 03_Sviluppo_Web_e_Database/05_Sviluppo_Applicativo_CRUD/blog_scolastico/app/schema.sql
+++ 03_Sviluppo_Web_e_Database/05_Sviluppo_Applicativo_CRUD/blog_scolastico/app/schema.sql
@@ -15,3 +15,12 @@ CREATE TABLE post (
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+INSERT INTO user (username, password) VALUES ('admin', 'adminpass');
+INSERT INTO user (username, password) VALUES ('student', 'studentpass');
+
+INSERT INTO post (author_id, title, body) VALUES (1, 'Welcome to the Blog', 'This is the first post on the blog!');
+INSERT INTO post (author_id, title, body) VALUES (2, 'Hello World', 'This is a post by a student.');
+INSERT INTO post (author_id, title, body) VALUES (1, 'Second Post', 'Another post by the admin user.');
+INSERT INTO post (author_id, title, body) VALUES (2, 'Learning SQL', 'This post discusses basic SQL commands.');
+INSERT INTO post (author_id, title, body) VALUES (1, 'Database Design', 'An introduction to designing databases.');
